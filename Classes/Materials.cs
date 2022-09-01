25d983d041424f5cd6cb1538ded21cf3d6336fee
willianWiegand <willian.wiegand@gmail.com>
2022-09-01 06:28:25 -0300

    Add files via upload

---
+++ Classes/Materials.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApontmentoWebAPI.Classes
+{
+    public class Materials
+    {
+        [JsonProperty(propertyName:"materialCode")]
+        public string MaterialCode { get; set; }
+        [JsonProperty(propertyName: "materialDescription")]
+        public string MaterialDescription { get; set; }
+
+    }
+}
---
+++ Classes/Orders.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+
+namespace ApontmentoWebAPI.Classes
+{
+    public class Orders
+    {
+        
+        [JsonProperty(propertyName: "order")]
+        public string Order { get; set; }
+        [JsonProperty(propertyName: "quantity")]
+        public int Quantity { get; set; }
+        [JsonProperty(propertyName: "productCode")]
+        public string ProductCode { get; set; }
+        [JsonProperty(propertyName: "productDescription")]
+        public string ProductDescription { get; set; }
+        [JsonProperty(propertyName: "image")]
+        public string Image { get; set; }
+        [JsonProperty(propertyName: "cycleTime")]
+        public double CycleTime { get; set; }
+        //[JsonProperty(propertyName: "materials")]
+        //public Materials Materials { get; set; }
+
+    }
+}
