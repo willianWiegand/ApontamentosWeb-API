52951a098359310c7a67649101c3ebcc43ddade6
WillianWiegand <willian.wiegand@gmail.com>
2022-09-05 17:28:59 -0300

    inicio refatoração da API

---
+++ Classes/WebAPI.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using ApontmentoWebAPI.Classes;
+
+namespace ApontmentoWebAPI.Classes
+{
+    class WebAPI
+    {
+        public string URL { get; set; }
+        public APIType Type { get; set; }
+
+        public WebAPI(string url, APIType type)
+        {
+            URL = url;
+            Type = type;
+           
+            switch (Type)
+            {
+                case APIType.Production:
+                    URL += "SetProduction";
+                break;
+
+                case APIType.OrderList:
+                    URL += "GetOrders";
+                break;
+            }
+
+        }
+
+        public void GetOrder(OrderList orderList)
+        {
+
+        }
+    }
+}
